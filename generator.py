2a378508b2520777804f44158943f225d2f44c23
Hercsel Péter <dontrajik97@gmail.com>
2022-12-19 23:38:13 +0100

    Final Commit

--- generator.py
+++ generator.py
@@ -23,14 +23,31 @@ def generateRandomPicture(noise):
         x2 = randint(0, 512)
         y1 = randint(0, 512)
         y2 = randint(0, 512)
-        draw.line((x1, x2, y1, y2), width=randint(1, 30), fill=(randint(0, 255), randint(0, 255), randint(0, 255)))
+        draw.line((x1, x2, y1, y2), width=randint(1, 30), fill=(
+            randint(0, 255), randint(0, 255), randint(0, 255)))
 
-    img2 = Image.open("barcode.png").convert("RGB")
-    img2 = img2.rotate(randint(0, 360), expand=1, fillcolor=(r, g, b))
+    img2 = Image.open("barcode.png").convert("RGBA")
+    barcodeWhere = Image.open("barcode.png").convert("RGBA")
+    draw2 = ImageDraw.Draw(barcodeWhere)
+    draw2.rectangle([(0, 0), barcodeWhere.size], fill=(255, 255, 255))
+    barcodeWhere.save("asd.png", 'PNG')
+
+    angle = randint(0, 360)
+    img2 = img2.rotate(angle, expand=1)
+    barcodeWhere = barcodeWhere.rotate(angle, expand=1)
     w = randint(0, 512-img2.size[0])
     h = randint(0, 512-img2.size[1])
-    img.paste(img2, (w, h))
+    img.paste(img2, (w, h), img2)
 
-    img.save(''+str(w) + "_" + str(h)+".png", 'PNG')
+    savename = "{}_{}_{}_{}_{}.png".format(
+        w, h, img2.size[0], img2.size[1], angle)
+    img.save(savename, 'PNG')
+    draw.rectangle([(0, 0), img.size], fill=(0, 0, 0))
+    img.paste(barcodeWhere, (w, h), barcodeWhere)
 
-    os.system("rm new_code.svg barcode.png ")
+    savename = "{}_{}_{}_{}_{}_test.png".format(
+        w, h, img2.size[0], img2.size[1], angle)
+    img = img.convert("RGB")
+    img.save(savename, 'PNG')
+
+    os.system("rm new_code.svg barcode.png asd.png")
--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from generator import generateRandomPicture
 import os
 from random import randint
 
-for i in range(50):
-    generateRandomPicture(randint(0, 50))
+output = open("result.csv", "w")
+
+for i in range(40):
+    noise = randint(0, 50)
+    generateRandomPicture(noise)
     for x in os.listdir(os.getcwd()):
-        if x.endswith(".png"):
-            testPicture = x
-    image = cv2.imread(testPicture)
-    os.system("rm " + testPicture)
+        if x.endswith("test.png"):
+            testPicture = x[:-9]
+    image = cv2.imread(testPicture+".png")
+    inputBoundingBox = cv2.imread(testPicture+"_test.png")
+    os.system("rm " + testPicture+".png " + testPicture + "_test.png")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     edges = cv2.Canny(gray, 400, 500)
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 40,minLineLength=15, maxLineGap=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 40,
+                            minLineLength=15, maxLineGap=3)
 
     linesImage = np.zeros((image.shape[0], image.shape[1], 3), np.uint8)
 
@@ -29,15 +34,27 @@ for i in range(50):
 
     edges = cv2.Canny(linesImage, 30, 200)
 
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(
+        edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     contour = max(contours, key=cv2.contourArea)
 
     rect = cv2.minAreaRect(contour)
+    boundingBox = np.zeros_like(image).astype(np.uint8)
     box = np.int0(cv2.boxPoints(rect))
     cv2.drawContours(image, [box], 0, (36, 255, 12), 3)
+    cv2.drawContours(boundingBox, [box], 0,
+                     (255, 255, 255), thickness=cv2.FILLED)
+    boundingBox = boundingBox.astype(np.uint8)
 
-    cv2.imshow("result", image)
-    cv2.waitKey()
+    thresh1 = cv2.threshold(boundingBox, 120, 255, cv2.THRESH_BINARY)[1]
+    thresh2 = cv2.threshold(inputBoundingBox, 120, 255, cv2.THRESH_BINARY)[1]
 
-    cv2.destroyAllWindows()
+    bitwiseAnd = cv2.bitwise_and(thresh1, thresh2)
+
+    matchingPercentage = np.sum(bitwiseAnd)*100/np.sum(inputBoundingBox)
+    print("{:0.2f}%".format(matchingPercentage))
+    output.write("{},{:0.2f}%\n".format(noise, matchingPercentage))
+
+
+output.close()
